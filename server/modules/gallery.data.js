d4990e2581a6fe316e40349118e9076edb2263eb
Everett Butler <everettjamesbutler@gmail.com>
2023-04-02 17:22:43 -0500

    Fnished pathing all of my photos and adding them to the array!

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,10 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
+    { id: 2, path: 'images/Christmas_Sephora.jpg', description: 'Photo of Sephora, one of my cats, taken around Christmas-time', likes: 0 },
+    { id: 3, path: 'images/E_headshot_1.jpeg', description: 'One of my headshots taken last winter!', likes: 0 },
+    { id: 4, path: 'images/JJ_&_Sephora.png', description: 'JJ & Sephora pictured in our Catio', likes: 0 },
+    { id: 5, path: 'images/JJs_Nose.jpg', description: `JJ's Nose crunching when he scratches an itch!`, likes: 0 }
+
 ];
 
 module.exports = galleryItems;
