013cc754e4bbc91ea4710ff560f0473fdc62daeb
Everett Butler <everettjamesbutler@gmail.com>
2023-04-11 11:42:26 -0500

    got all my photos resized. working on like functionality now!

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,9 +1,13 @@
 const galleryItems = [
     { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
-    { id: 2, path: 'images/Christmas_Sephora.jpg', description: 'Photo of Sephora, one of my cats, taken around Christmas-time', likes: 0 },
-    { id: 3, path: 'images/E_headshot_1.jpeg', description: 'One of my headshots taken last winter!', likes: 0 },
-    { id: 4, path: 'images/JJ_&_Sephora.png', description: 'JJ & Sephora pictured in our Catio', likes: 0 },
-    { id: 5, path: 'images/JJs_Nose.jpg', description: `JJ's Nose crunching when he scratches an itch!`, likes: 0 }
+    { id: 2, path: 'images/Christmas_Sephora_1.png', description: 'Photo of Sephora, one of my cats, taken around Christmas-time', likes: 0 },
+    { id: 3, path: 'images/Edema_Ruh_1.png', description: 'The cover of a bomb album by three awesome artists!', likes: 0 },
+    { id: 4, path: 'images/Ceremony_1.jpg', description: 'Ceremony, by goth band: Twin Tribes! Bangers all the way through', likes: 0 },
+    { id: 5, path: 'images/forgetmenot_1.png', description: `forgetmenot is a spinoff album from Weatherday`, likes: 0 },
+    { id: 6, path: 'images/JJ_Nose_1.png', description: `JJ's cute nose crinkle!`, likes: 0 },
+    { id: 7, path: 'images/JJ&Sephora_1.png', description: `JJ & Sephora! In our little Catio!`, likes: 0 },
+    { id: 8, path: 'images/Porcelain_songs_1.png', description: `A compilation from Weatherday's discord`, likes: 0 },
+    { id: 9, path: 'images/Shadows_1.jpg', description: `Twin Tribes' first album! What a ride!!`, likes: 0 }
 
 ];
 
